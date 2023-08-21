f7632a99f4afc16fbf8da2d423807b7f7180ebca
Luka Mendonça <lukamendoncadev@proton.me>
2023-08-20 21:11:30 -0300

    config js for toogle menu btn

---
+++ app.js
@@ -0,0 +1,13 @@
+const hamburguerBtn = document.querySelector('.hamburguer');
+const menu = document.querySelector('.menu');
+
+addEventListener('click', () => {
+    menu.setAttribute('data-open', 
+    menu.getAttribute('data-open') === 'true' ? 'false' : 'true');
+    hamburguerBtn.classList.toggle('open');
+    //if (menu.getAttribute('data-open') === 'true') {
+    //    menu.setAttribute('data-open', 'false');
+    //} else {
+    //    menu.setAttribute('data-open', 'true');
+    //}
+});
